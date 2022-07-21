1b606149fb06a52d108f405686e5c68e1be977e7
Niezwan <nzwnabdulwahid@gmail.com>
2022-07-21 17:08:09 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Niezwan Abdul Wahid on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
